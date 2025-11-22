4f8ecd104289a7af4d68c9c0256e672d12221f3e
Lerato Mokaloba <mokalobalerato@gmail.com>
2025-11-22 02:08:29 +0200

    replaced the app.jsx import with router.jsx import to main entry file so the app can load all pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Router from './Router.jsx'
+
+// This is where the React app starts running.
+// We find the <div id="root"> in index.html
+// and tell React to show our app inside it.
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* Load all the routes for the app */}
+    <Router />
   </StrictMode>,
 )

c74f4b1239c3998d7647aa198998ffafa545a336
Lerato Mokaloba <mokalobalerato@gmail.com>
2025-11-22 02:33:28 +0200

    Update ShowDetail.jsx

--- src/pages/ShowDetail.jsx
+++ src/pages/ShowDetail.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
-import { useParams, useNavigate, useLocation } from 'react-router-dom';
-import Header from '../components/Header';
-import styles from './ShowDetail.module.css';
-
 // useParams = get the id from the URL
 // useNavigate = move between pages
 // useLocation = get extra data from the previous page
 // styles imported from the CSS module for this page
+
+import { useEffect, useState } from 'react';                            
+import { useParams, useNavigate, useLocation } from 'react-router-dom';
+import Header from '../components/Header';                             
+import styles from './ShowDetail.module.css';                          
+
